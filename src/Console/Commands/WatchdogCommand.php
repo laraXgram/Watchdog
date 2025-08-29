2c747a43b55cc7ef0c6b8a9a7329d1a155626109
AmirHkrg <amirh.kargar85@gmail.com>
2025-08-29 17:22:03 +0330

    chore: add watchdog command

---
+++ src/Console/Commands/WatchdogCommand.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace LaraGram\Watchdog\Console\Commands;
+
+use LaraGram\Console\Command;
+use LaraGram\Process\Exceptions\ProcessTimedOutException;
+use LaraGram\Watchdog\File;
+use LaraGram\Watchdog\Guards\EnsurePcntlIsAvailable;
+use LaraGram\Watchdog\Options;
+use LaraGram\Watchdog\ProcessFactory;
+use LaraGram\Console\Attribute\AsCommand;
+use LaraGram\Console\Process\Exception\ProcessSignaledException;
+
+use function LaraGram\Console\Prompts\Convertor\render;
+use function LaraGram\Console\Prompts\Convertor\renderUsing;
+
+#[AsCommand(name: 'watchdog')]
+class WatchdogCommand extends Command
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected $signature = 'watchdog
+        {--filter= : Filter the logs by the given value}
+        {--message= : Filter the logs by the given message}
+        {--level= : Filter the logs by the given level}
+        {--user= : Filter the logs by the given authenticated ID}
+        {--timeout=3600 : The maximum execution time in seconds}';
+
+    /**
+     * {@inheritDoc}
+     */
+    protected $description = 'Tails the application logs.';
+
+    /**
+     * The file instance, if any.
+     */
+    protected ?File $file = null;
+
+    /**
+     * Handles the command execution.
+     */
+    public function handle(ProcessFactory $processFactory): void
+    {
+        EnsurePcntlIsAvailable::check();
+
+        renderUsing($this->output);
+        render(<<<'HTML'
+            <div class="max-w-150 mx-2 mt-1 flex">
+                <div>
+                    <span class="px-1 bg-blue uppercase text-white">INFO</span>
+                    <span class="flex-1">
+                        <span class="ml-1 ">Tailing application logs.</span>
+                    </span>
+                </div>
+                <span class="flex-1"></span>
+                <span class="text-gray ml-1">
+                    <span class="text-gray">Press Ctrl+C to exit</span>
+                </span>
+            </div>
+            HTML,
+        );
+
+        render(<<<'HTML'
+            <div class="max-w-150 mx-2 flex">
+                <div>
+                </div>
+                <span class="flex-1"></span>
+                <span class="text-gray ml-1">
+                    <span class="text-gray">Use -v|-vv to show more details</span>
+                </span>
+            </div>
+            HTML,
+        );
+
+        $this->file = new File(storage_path('watchdog/'.uniqid().'.watchdog'));
+        $this->file->create();
+        $this->trap([SIGINT, SIGTERM], fn () => $this->file->destroy());
+
+        $options = Options::fromCommand($this);
+
+        assert($this->file instanceof File);
+
+        try {
+            $processFactory->run($this->file, $this->output, $this->laragram->basePath(), $options);
+        } catch (ProcessSignaledException $e) {
+            if (in_array($e->getSignal(), [SIGINT, SIGTERM], true)) {
+                $this->newLine();
+            }
+        } catch (ProcessTimedOutException $e) {
+            $this->components->info('Maximum execution time exceeded.');
+        } finally {
+            $this->file?->destroy();
+        }
+    }
+
+    /**
+     * Handles the object destruction.
+     */
+    public function __destruct()
+    {
+        if ($this->file) {
+            $this->file->destroy();
+        }
+    }
+}
