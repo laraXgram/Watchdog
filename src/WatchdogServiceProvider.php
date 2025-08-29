3380157e01047668f3254ce60add96c779de9056
AmirHossein <amirh.kargar@gmail.com>
2025-08-29 15:47:17 +0330

    chore: add main files

---
+++ src/WatchdogServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace LaraGram\Watchdog;
+
+use LaraGram\Support\ServiceProvider;
+
+class WatchdogServiceProvider extends ServiceProvider
+{
+    /**
+     * Registers the application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstraps the application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
