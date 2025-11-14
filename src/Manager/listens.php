a44f7db420478e81bf2e36c5cc065365343bc425
AmirHkrg <amirh.kargar85@gmail.com>
2025-11-14 18:25:55 +0330

    chore: add listens name

--- src/Manager/listens.php
+++ src/Manager/listens.php
@@ -2,13 +2,14 @@
 
 use LaraGram\Support\Facades\Bot;
 use LaraGram\Watchdog\Manager\Controllers\LogManagerController;
+use LaraGram\Watchdog\Manager\Middleware\IsManagerAdmin;
 
 Bot::controller(LogManagerController::class)
     ->name('watchdog.')
     ->group(function () {
-        Bot::onCommand(config('watchdog.manager.command'), 'index');
-        Bot::onCallbackQueryData('watchdog_log_load_{pointerIndex}', 'loadFiles');
-        Bot::onCallbackQueryData('watchdog_log_read_{pointerIndex}', 'readFile');
-        Bot::onCallbackQueryData('watchdog_log_entry_{pointerIndex}_{entryIndex}', 'readEntry');
-        Bot::onCallbackQueryData('watchdog_log_delete_{pointerIndex}', 'deleteFile');
+        Bot::onCommand(config('watchdog.manager.command'), 'index')->name('index');
+        Bot::onCallbackQueryData('watchdog_log_load_{pointerIndex}', 'loadFiles')->name('load_files');
+        Bot::onCallbackQueryData('watchdog_log_read_{pointerIndex}', 'readFile')->name('read_file');
+        Bot::onCallbackQueryData('watchdog_log_entry_{pointerIndex}_{entryIndex}', 'readEntry')->name('read_entry');
+        Bot::onCallbackQueryData('watchdog_log_delete_{pointerIndex}', 'deleteFile')->name('delete_file');
     });
